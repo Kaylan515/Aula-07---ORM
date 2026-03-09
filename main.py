e7c9c0fa78585d68a203f0d2e4217943ee373f7c
Kaylan <kaylanteodoro344@gmail.com>
2026-03-09 12:45:40 -0300

    Criando Estoque.db ✔👍

--- main.py
+++ main.py
@@ -9,3 +9,45 @@ from sqlalchemy.orm import declarative_base
 
 # Importar a ferramenta para criar sessões de banco
 from sqlalchemy.orm import sessionmaker
+
+# Criar classe base do ORM
+Base = declarative_base()
+
+# Classe = tabela
+# Objeto = linha da tabela
+# Atributo = coluna da tabela
+
+# Classe Produto representa a tabela no banco de dados
+class Produto(Base):
+    #nome da tabela no banco
+    __tablename__ = "produtos"
+
+    # Coluna id
+    # Integer = Número inteiro
+    # primary_key = True
+    id = Column(Integer, primary_key=True)
+    
+    # Nome do produto
+    # String > texto
+    nome = Column(String(100))
+
+    # Preço do produto
+    # Float = número decimal
+    preco = Column(Float)
+
+    # Método construtor
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+    
+    #Representação do objeto para imprimir
+    def __repr__(self):
+        return f"Produto(id={self.id}, nome='{self.nome}', preco={self.preco})"
+    
+# Criar a conexão com sqlite
+# echo=True = log do sql
+engine = create_engine("sqlite:///estoque.db", echo=True)
+
+# Criar as tabelas no banco se não existirem
+Base.metadata.create_all(engine)
+
